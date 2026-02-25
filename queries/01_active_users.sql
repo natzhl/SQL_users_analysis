cb3448f277699aadcfbf768c35f8e6190341efc4
Natalia Zheleznyak <109067095+natzhl@users.noreply.github.com>
2026-02-25 15:34:30 +0300

    Update 01_active_users.sql

--- queries/01_active_users.sql
+++ queries/01_active_users.sql
@@ -1,5 +1,9 @@
+-- MAU (monthly active users)
+-- активные = те, которые совершили событие (event)
 SELECT
+  -- выбираем месяц и группируем события по месяцу
   FORMAT_DATE('%Y-%m', DATE(created_at)) AS month,
+  -- считаем уникальных пользователей
   COUNT(DISTINCT user_id) AS active_users
 FROM `bigquery-public-data.thelook_ecommerce.events`
 GROUP BY month
