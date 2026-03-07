7e4a463d6afdc6cb86df2c8727fef2860a9da94d
Natalia Zheleznyak <109067095+natzhl@users.noreply.github.com>
2026-03-07 16:48:36 +0300

    Update 08_margin.sql

--- queries/08_margin.sql
+++ queries/08_margin.sql
@@ -1,3 +1,4 @@
+-- считаем маржинальность (доходность бизнеса)
 SELECT
     SUM(oi.sale_price) AS revenue,
     SUM(oi.sale_price * 0.6) AS cost,
@@ -7,3 +8,30 @@ FROM `bigquery-public-data.thelook_ecommerce.order_items` oi
 JOIN `bigquery-public-data.thelook_ecommerce.orders` o
 ON oi.order_id = o.order_id
 WHERE o.status = 'Complete'
+
+-- маржинальность по товарам (топ 20 самых прибыльных товаров)
+SELECT
+    oi.product_id,
+    SUM(oi.sale_price) AS revenue,
+    SUM(oi.sale_price * 0.4) AS profit,
+    SUM(oi.sale_price * 0.4) / SUM(oi.sale_price) AS margin
+FROM `bigquery-public-data.thelook_ecommerce.order_items` oi
+JOIN `bigquery-public-data.thelook_ecommerce.orders` o
+ON oi.order_id = o.order_id
+WHERE o.status = 'Complete'
+GROUP BY oi.product_id
+ORDER BY profit DESC
+LIMIT 20
+
+-- маржинальность по месяцам
+SELECT
+    FORMAT_TIMESTAMP('%Y-%m', o.created_at) AS month,
+    SUM(oi.sale_price) AS revenue,
+    SUM(oi.sale_price * 0.4) AS profit,
+    SUM(oi.sale_price * 0.4) / SUM(oi.sale_price) AS margin
+FROM `bigquery-public-data.thelook_ecommerce.order_items` oi
+JOIN `bigquery-public-data.thelook_ecommerce.orders` o
+ON oi.order_id = o.order_id
+WHERE o.status = 'Complete'
+GROUP BY month
+ORDER BY month
